f4484923398b4637c5cc7d5cf4b2c76beaa766da
varunjha-dev <varunjha.dev@gmail.com>
2024-12-06 19:00:24 +0530

    added some functionality

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,24 @@
 import { IMG_CDN_URL } from "../utils/constants";
 
-const MovieCard = ({ posterPath }) => {
-  if (!posterPath) return null;
+const MovieCard = ({ posterPath, originalTitle }) => {
+  if (!posterPath || !originalTitle) return null;
+
+  const handleClick = () => {
+    // Encode the movie title for URL
+    const searchQuery = encodeURIComponent(originalTitle);
+
+    // Choose the search URL (Google or YouTube)
+    const youtubeSearchUrl = `https://www.youtube.com/results?search_query=${searchQuery}+trailer in hindi or enlish only`;
+
+
+    window.open(youtubeSearchUrl, "_blank");  // Open YouTube search
+  };
+
   return (
-    <div className="w-36 md:w-48 pr-4">
+    <div className="w-36 md:w-48 pr-4 cursor-pointer" onClick={handleClick}>
       <img alt="Movie Card" src={IMG_CDN_URL + posterPath} />
     </div>
   );
 };
+
 export default MovieCard;
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,12 +2,25 @@ import MovieCard from "./MovieCard";
 
 const MovieList = ({ title, movies }) => {
   return (
-    <div className="my-8 ">
+    <div className="my-8">
       <h2 className="text-lg md:text-3xl py-4 text-white mb-4">{title}</h2>
-      <div style={{ display: 'flex', overflowX: 'scroll', WebkitOverflowScrolling: 'touch', msOverflowStyle: 'none', scrollbarWidth: 'none' }} className="space-x-4">
-        <div className="flex ">
+      <div
+        style={{
+          display: "flex",
+          overflowX: "scroll",
+          WebkitOverflowScrolling: "touch",
+          msOverflowStyle: "none",
+          scrollbarWidth: "none",
+        }}
+        className="space-x-4"
+      >
+        <div className="flex">
           {movies?.map((movie) => (
-            <MovieCard key={movie.id} posterPath={movie.poster_path} />
+            <MovieCard
+              key={movie.id}
+              posterPath={movie.poster_path} // Movie poster path
+              originalTitle={movie.original_title} // Movie title for opening a new tab
+            />
           ))}
         </div>
       </div>
@@ -15,4 +28,5 @@ const MovieList = ({ title, movies }) => {
     </div>
   );
 };
+
 export default MovieList;
